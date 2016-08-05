1be38e5f3c3a875534a0db95b80596eaed80190b
ning00 <872245994@qq.com>
2016-08-05 11:03:51 +0800

    11

--- pattern.js
+++ pattern.js
@@ -1,1 +1,14 @@
-var pattern = 'xxx';
+/****
+*正则验证
+*/
+
+
+/*示例*/
+//1、端口号（0~65535）
+var post = 4;
+var result = 4>3?true:false;
+// var result = port>=0 && port <=65535 ? true:false;
+// var pattern = //
+console.log(result);
+
+/*规则*/
