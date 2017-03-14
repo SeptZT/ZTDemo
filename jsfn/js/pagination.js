009a49d9d21bcd8eaeda52bacd2c9455511552b6
ning00 <872245994@qq.com>
2017-03-14 11:52:17 +0800

    修改翻页插件

--- jsfn/js/pagination.js
+++ jsfn/js/pagination.js
@@ -1,29 +1,31 @@
-
-function Page(ele, totalPage, handle) {
+function Page(ele, total, handle) {
   this.obj = $(ele);
+  this.total = total;
   this.handle = handle;
-
-  this.nowPage = 1; // 当前选中页
-  this.pageCount = 10;  // 显示页码的数量：1到10
-
-  this._init(Math.ceil(totalPage)); // 传入总页数(向上取整，避免传入的数是小数)
+  
+  this.nowPage = 1; // 设置当前选中页
+  this.pageCount = 10;  // 设置显示页码的数量
+  
+  this._init(); 
 }
 
 /* *
  * 生成页码
  * */
-Page.prototype._init = function(total) {
+Page.prototype._init = function() {
   var startNum, endNum,
-      startClass = "",  // 上一页、第一页的样式
-      endClass = "",  // 下一页、最后一页的样式
+      total = this.total,
+      startClass = '',  // 上一页、第一页的样式
+      endClass = '',  // 下一页、最后一页的样式
       tmp = total - this.nowPage,
-      html = [];
-  
+      html = [],
+      totalPage = Math.ceil(total / this.pageCount);  // 总页数(向上取整，避免传入的数是小数)
+
   // 判断页码显示的起始数字startNum和结束数字startNum
-  if(total <= this.pageCount) {
+  if (total <= this.pageCount) {
     startNum = 1;
     endNum = total;
-  } else if(tmp >= this.pageCount) {
+  } else if (tmp >= this.pageCount) {
     startNum = this.nowPage;
     endNum = this.nowPage + this.pageCount - 1;
   } else {
@@ -31,29 +33,28 @@ Page.prototype._init = function(total) {
     endNum = total;
   }
 
-  if(this.nowPage === 1) {
-    startClass = "page-disabled";
-  } else if(this.nowPage === total) {
-    endClass = "page-disabled";
+  if (this.nowPage === 1) {
+    startClass = ' page-disabled';
+  } else if (this.nowPage === total) {
+    endClass = ' page-disabled';
+  }
+  
+  html.push('<li class="page-change' + startClass + '" title="第一页" index="page-first">&lt;&lt;</li>');
+  html.push('<li class="page-change' + startClass + '" title="上一页" index="page-prev">&lt;</li>');
+
+  for (var i = startNum; i <= endNum; i++) {
+    html.push('<li class="page-num ' + (this.nowPage === i ? 'page-selected' : '') + '" index="' + i + '">' + i + '</li>');
   }
 
-  html.push("<a class='" + startClass + "' href='javascript:void(0);' title='第一页' index='page-first'>&lt;&lt;</a>");
-  html.push("<a class='" + startClass + "' href='javascript:void(0);' title='上一页' index='page-prev'>&lt;</a>");
+  html.push('<li class="page-change' + endClass + '" title="下一页" index="page-next">&gt;</li>');
+  html.push('<li class="page-change' + endClass + '" title="最后一页" index="page-last">&gt;&gt;</li>');
+  html.push('<li class="page-search"><input placeholder="搜索"/><button>搜索</button><span>共' + totalPage + '页'+ total +'条</span></li>');
 
-  for(var i = startNum; i <= endNum; i++) {
-    html.push("<a class='" + (this.nowPage === i ? "page-selected" : "") + "' href='javascript:void(0);' index='" + i + "'>" + i + "</a>");
-  }
-
-  html.push("<a class='" + endClass + "' href='javascript:void(0);' title='下一页' index='page-next'>&gt;</a>");
-  html.push("<a class='" + endClass + "' href='javascript:void(0);' title='最后一页' index='page-last''>&gt;&gt;</a>");
-  html.push("<input class='page-input' type='text'/><button>搜索</button><span>" + total + "页</span>");
-
-  this.obj.html(html.join(""));
+  this.obj.html('<ul>' + html.join('') + '</ul>');
 
   this._bind();
+  this._goTo();  
   this._enter();
-  this._goTo(total);
-  
 };
 
 /* *
@@ -63,76 +64,72 @@ Page.prototype._bind = function() {
   var _this = this,
     handle = this.handle;
 
-  if(!handle || typeof handle !== "function") {
-    console.warn("no callback bind to pagination！");
+  if (!handle || typeof handle !== 'function') {
+    console.warn('no callback bind to pagination！');
     return;
   }
-  this.obj.off("click").on("click", function(e) {
-    var e = e || window.event,
-      target = e.target,
-      index = $(target).attr("index"),
-      className = target.className;
 
-    if(target.tagName === "BUTTON") {
-      _this._goTo(target, handle);
-    }
-
-    if(target.tagName !== "A" || className === "selected" || className === "page-disabled") {
-      return;
-    }
-
-    // 上一页
-    if(index == "prev") {
-      if(_this.nowPage === 1) {
-        return;
-      }
-      handle(--_this.nowPage);
-      return;
-    }
-    // 下一页
-    if(index == "page-next") {
-      if(_this.nowPage == _this.pageCount) {
-        return;
-      }
-      handle(++_this.nowPage);
-      return;
-    }
-    // 第一页
-    if(index == "page-first") {
-      handle(_this.nowPage = 1);
-      return;
-    }
-    // 最后一页
-    if(index == "page-last") {
-      handle(_this.nowPage = _this.pageCount);
+  // 点击数字翻页
+  this.obj.find('.page-num').off('click').on('click', function() {
+    if ($(this).hasClass('page-selected')) {
       return;
     }
     
-    // 数字翻页
-    handle(_this.nowPage = parseInt(index));
+    _this.nowPage = parseInt($(this).attr('index'));
+    handle(_this.nowPage);
+    _this._init();
+  });
+
+  // 点击切换上一页、下一页、第一页、最后一页
+  this.obj.find('.page-change').off('click').on('click', function() {
+    var index = $(this).attr('index');
+
+    if ($(this).hasClass('page-disabled')) {
+      return;
+    }
+    
+    switch (index) {
+      case 'page-first':
+        _this.nowPage = 1;
+        break;
+      case 'page-prev':
+        _this.nowPage--;
+        break;
+      case 'page-next':
+        _this.nowPage++;
+        break;
+      case 'page-last':
+        _this.nowPage = _this.total;
+        break;
+    }
+    handle(_this.nowPage);
+    _this._init();
   });
 };
 
 /* *
- * 搜索
+ * 点击搜索
  * */
-Page.prototype._goTo = function(total) {
-  var obj = this.obj,
+Page.prototype._goTo = function() {
+  var obj = this.obj.find('.page-search'),
       _this = this;
-  obj.find('button').on('click', function(){
-    var val = $('.page-input').val();
+      
+  obj.find('button').off('click').on('click', function(){
+    var val = obj.find('input').val();
 
-    if(isNaN(val) || val < 1 || val > total || !(/^\d+$/.test(val))) {
+    if (isNaN(val) || val < 1 || val > _this.total || !(/^\d+$/.test(val))) {
       obj.append('<span class="err_tip">输入有误</span>');
       setTimeout(function() {
         obj.find('.err_tip').fadeOut();
       }, 1500);
   
-      obj.find('.page-input').val("");
+      obj.find('input').val('');
       return;
     }
-  
-    _this.handle(_this.nowPage = parseInt(val));
+    
+    _this.nowPage = parseInt(val);
+    _this.handle(_this.nowPage);
+    _this._init();
   });
 };
 
@@ -141,32 +138,30 @@ Page.prototype._goTo = function(total) {
  * */
 Page.prototype._enter = function() {
   var _this = this;
-  $(this).find('.page-input').on("keyup", function(e) {
-    var e = e || window.event;
-    if(e.keyCode == 13) {
+  $(this).find('.page-search input').on('keyup', function(e) {
+    e = e || window.event;
+    if (e.keyCode === 13) {
       _this._goTo();
     }
   });
 };
 
-
-;+function($) {
+(function($) {
   $.fn.extend({
     /* *
      * 调用方法:
-     * $ele.pageInit(totalPage, handle);
+     * $ele.pageInit(total, handle);
      *
      * 参数说明:
-     * @param   totalPage [总页数]
-     * @param   handle    [回调函数多为获取翻页数据的方法]
+     * @param   total [总条数]
+     * @param   handle    [回调函数为获取翻页数据的方法]
      */
     pageInit: function(totalPage, handle) {
-      if(!totalPage) {
-        $(this).html("");
+      if (!totalPage) {
+        $(this).html('');
       } else {
         new Page(this, totalPage, handle);
       }
-
       return this;
     },
     /* *
@@ -182,5 +177,5 @@ Page.prototype._enter = function() {
       this.nowPage = 1;
       return this;
     }
-  })
-}(jQuery);
+  });
+})(jQuery);
